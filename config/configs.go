8890eae2ac2156028c57dcf50068ff224693a838
Ali Pourhabibi <ali.pourhabibi1379@gmail.com>
2024-07-12 10:22:36 +0330

    feat: add config and hot reloading
    Add config for port and logging
    Add hot reloading the configs
    Add config doc to README

---
+++ config/configs.go
@@ -0,0 +1,40 @@
+package config
+
+import (
+	"log/slog"
+	"os"
+
+	"gopkg.in/yaml.v3"
+)
+
+var MapLevel = map[string]slog.Level{
+	"DEBUG": slog.LevelDebug,
+	"INFO":  slog.LevelInfo,
+	"WARN":  slog.LevelWarn,
+	"ERROR": slog.LevelError,
+}
+
+var Confs config
+
+type config struct {
+	Server server `yaml:"server"`
+	Logger logger `yaml:"logger"`
+}
+
+type server struct {
+	Port uint `yaml:"port"`
+}
+
+type logger struct {
+	AddSource bool   `yaml:"add_source"`
+	Level     string `yaml:"level"`
+}
+
+func Load(path string) error {
+	f, err := os.ReadFile(path)
+	if err != nil {
+		return err
+	}
+
+	return yaml.Unmarshal(f, &Confs)
+}
